4a579842da1e019fe6dbe659598a47f38e410df4
charlotteraisch <67684420+charlotteraisch@users.noreply.github.com>
2020-07-15 11:47:06 +0200

    Start

---
+++ SF/360/data.js
@@ -0,0 +1,316 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-viewport1_1ebene-3",
+      "name": "Viewport1_1Ebene-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.06310129620818827,
+        "pitch": 0.16587882105006102,
+        "fov": 1.5824035684662277
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07807959793679053,
+          "pitch": 0.1767812618210911,
+          "rotation": 12.566370614359176,
+          "target": "2-vp3-2"
+        },
+        {
+          "yaw": 1.543528356707867,
+          "pitch": 0.16776255380155902,
+          "rotation": 5.497787143782138,
+          "target": "1-vp2-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0240116010343225,
+          "pitch": 0.08529461439359665,
+          "title": "Augmented Reality",
+          "text": "<div>When you enter the school, put on your AR glasses to intersct with all the hidden features in this building.</div>"
+        },
+        {
+          "yaw": 0.44944771728270005,
+          "pitch": 0.7427975686890651,
+          "title": "2D Interface",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.659392609587762,
+          "pitch": 1.2644938506866694,
+          "title": "2D Interface info",
+          "text": "You can pull out your 2D weekplan to organize your daily and weekly options."
+        },
+        {
+          "yaw": -0.7653228041556783,
+          "pitch": 1.1706022136265535,
+          "title": "3D Interface",
+          "text": "The 3D tree shows your learning path. Every week you can choose on which topics you want to focus."
+        },
+        {
+          "yaw": -1.0184864796292281,
+          "pitch": 1.031380539773453,
+          "title": "3D Interface",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6212739187873915,
+          "pitch": 0.1002524735631578,
+          "title": "Movement &amp; Interaction",
+          "text": "The dynamic shape of the building helps you to stay interactive and spontaneously physical movements are connected to the virtual ones. An overlaying AR Interface replaces your school bag. You can access this 2 Dor 3D Interface at the prepared spaces within the building. Architecture builds the new virtual Desktop."
+        },
+        {
+          "yaw": 2.752216673802929,
+          "pitch": -0.8077228439418906,
+          "title": "*",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-vp2-2",
+      "name": "VP2-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.4295518093893378,
+        "pitch": 0.26976397981165334,
+        "fov": 1.5824035684662277
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11854657976844862,
+          "pitch": 0.12831028014946888,
+          "rotation": 11.780972450961727,
+          "target": "0-viewport1_1ebene-3"
+        },
+        {
+          "yaw": 1.4412525188042995,
+          "pitch": 0.17142223313015137,
+          "rotation": 0,
+          "target": "2-vp3-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2772313721772903,
+          "pitch": -0.126744948058235,
+          "title": "Hi, Future School",
+          "text": "Check the concept of the building and the learning space typologies!"
+        },
+        {
+          "yaw": 0.1458041348441821,
+          "pitch": 0.024568228474088016,
+          "title": "Background",
+          "text": "An analysis of the existing learning spaces in schools help to find new, more dynamic spaces with a focus on individualized learning."
+        },
+        {
+          "yaw": 0.26315339000916715,
+          "pitch": 0.02894782849534927,
+          "title": "Concept",
+          "text": "The Future School concept is based on new diverse learning space typologies. Every student can decide individually how to learn an in which surrounding."
+        },
+        {
+          "yaw": 0.4061901975318758,
+          "pitch": 0.03133665630212157,
+          "title": "Building",
+          "text": "In relation to the location and the environment, the spaces can be organized differently."
+        },
+        {
+          "yaw": 0.8918939428520289,
+          "pitch": -0.3100940280197264,
+          "title": "Future School&nbsp;<div>Concept</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.104607835805103,
+          "pitch": -0.2994975921112317,
+          "title": "Future School&nbsp;<div>Typologies</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.8362244265674432,
+          "pitch": -0.03092563318432795,
+          "title": "Building site",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.5316459238670364,
+          "pitch": 0.12714579418006267,
+          "title": "\"Hands-On\" and \"White-Cave\"",
+          "text": "The second part of the building is non-virtual. Your learning tools, as for example your hands, stay physical.<br>Both areas overlay eachother and fire interaction on both levels. An example that could develop is vertical gardening. It is dependent on botanical and scientific knowledge as well as technical programming."
+        },
+        {
+          "yaw": -2.8444897546008256,
+          "pitch": 0.21237656440329644,
+          "title": "MEDIA workshop",
+          "text": "Recentls there is going on a workshop which connects 3 of the 6 main education topics; MEDIA, SCIENCE and HANDS-ON.<br><br>The 5 education topics are:<br><br>LANGUAGE<div>CULTURE &amp; SOCIETY</div><div>SCIENCE<br>MOVEMENT</div><div>MEDIA</div><div>HANDS-ON</div>"
+        },
+        {
+          "yaw": -2.1842432509538554,
+          "pitch": 0.2541084827629092,
+          "title": "Mutli-Use Glas Space",
+          "text": "This multi-use space is half public. Every student that passes by can check the workshops or events that are recently happening."
+        },
+        {
+          "yaw": -0.7965727697046034,
+          "pitch": -0.18770721088542075,
+          "title": "Background - Future?",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4674734331039847,
+          "pitch": -0.190488197906042,
+          "title": "Background&nbsp;<div>- Future Education</div>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-vp3-2",
+      "name": "VP3-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5666130420819133,
+          "pitch": 0.08472501456146375,
+          "rotation": 5.497787143782138,
+          "target": "0-viewport1_1ebene-3"
+        },
+        {
+          "yaw": -0.027687020167658005,
+          "pitch": 0.20617897981789213,
+          "rotation": 0,
+          "target": "1-vp2-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8775779596360103,
+          "pitch": 0.38329747162147676,
+          "title": "Cockpit Mantis Iris&nbsp;<div>- Concept</div>",
+          "text": "The Cockpit Mantis Iris is a movable object in which you can work individually and concentrated. Moreover it can connect with others physically and virtually. Group works with virtuals next to physicals are possible."
+        },
+        {
+          "yaw": -0.8679694023503686,
+          "pitch": -0.1442861711208927,
+          "title": "Cockpit Mantis Iris&nbsp;<div>- for individual learning</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4398051323736052,
+          "pitch": -0.15267556007353456,
+          "title": "Cockpit Mantis Iris&nbsp;<div>- group work</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6677396959345376,
+          "pitch": -0.14896957006182276,
+          "title": "Cockpit Mantis Iris&nbsp;<div>- meeting and connecting</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.32370893302073256,
+          "pitch": -0.04298002412646085,
+          "title": "Ramps",
+          "text": "As the building has different learning offers, it is organized with a lot of ramps to provoke movement and interaction."
+        },
+        {
+          "yaw": 1.0236752870298318,
+          "pitch": 0.0027730294888321794,
+          "title": "Swipe between your screens!",
+          "text": "The guy is swiping the layout from the table to the wall."
+        },
+        {
+          "yaw": 1.1741418129744083,
+          "pitch": 0.6126208917748226,
+          "title": "Group area",
+          "text": "This area is an open space for group works. It offers different learning situations for differetn physical states - as low or high sitting and standing."
+        },
+        {
+          "yaw": 2.6624372260306277,
+          "pitch": 0.4695807184372871,
+          "title": "Screen Table Interface",
+          "text": "You can log-in on the table screen. It replaces your school bag."
+        },
+        {
+          "yaw": -3.0595538273482568,
+          "pitch": 0.1722817110151844,
+          "title": "Wall Interface",
+          "text": "This space has desks that are oriented to the sides. The wall can be used as a screen. You can place information 3D at the wall."
+        },
+        {
+          "yaw": -2.457172423290201,
+          "pitch": 0.0224055285246898,
+          "title": "Virtual listeners",
+          "text": "In the back of the room the teacher is able to interact with virtual listeners."
+        },
+        {
+          "yaw": -1.879574660667842,
+          "pitch": 0.114556786003849,
+          "title": "Presentation",
+          "text": "Here you can join presentations of a physical or virtual teacher."
+        },
+        {
+          "yaw": -1.4351287939066761,
+          "pitch": 0.05290061630597087,
+          "title": "Integrated Screen for the teacher",
+          "text": "The teacher has his/her screen within the structure of the room to control the presantation interface."
+        }
+      ]
+    }
+  ],
+  "name": "Future School",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
